c3eacd6f155bf82ee5f05d42000ea7c0408bc790
Andrija Despotovic <52492677+andrijad@users.noreply.github.com>
2026-03-23 22:25:58 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
   site: 'https://devsmachina.app',
   output: 'static',
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
